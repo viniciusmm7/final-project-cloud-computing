77cb330498675d599eaeebc043d982977bbd3571
viniciusmm7 <viniciusmm7@al.insper.edu.br>
2023-12-04 19:33:09 -0300

    removing SSH permission

--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -28,13 +28,6 @@ resource "aws_security_group" "ec2_sec_group" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
